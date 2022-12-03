ff91503cb718824ccea423130d40326c42961fc0
Łukasz Nowak <38426907+nluk@users.noreply.github.com>
2022-12-03 14:03:54 +0100

    Day 4 template

---
+++ src/main/kotlin/04.kt
@@ -0,0 +1,14 @@
+class Day4 {
+    fun part1() = solve(day = 4){ input ->
+
+    }
+
+    fun part2() = solve(day = 4){ input ->
+
+    }
+}
+
+fun main() = with(Day4()){
+    part1()
+    part2()
+}
